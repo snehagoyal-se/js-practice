9b25d1133eab766e8538bec3d799acfb15259fbe
snehagoyal-se <snehagoyal9084@gmail.com>
2026-07-14 10:56:31 +0000

    new variable learning

---
+++ 01_bsics/variable.js
@@ -0,0 +1,16 @@
+const accID="15523"
+var accEmail="sneha@google.com"
+let accPass="123456"
+accCity="Jaipur"
+let accState
+//accID =2 //Not allowed
+
+accEmail="sg@sg"
+accPass="21212458"
+accCity="Banagalore"
+
+/* Prefer not o use var
+becasue of issue in block and functional scope  
+*/
+
+console.table([accID,accEmail,accPass,accCity,accState])
