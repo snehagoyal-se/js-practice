f211b35c4a200bfdc71f13e3387fe578161ee01f
snehagoyal-se <snehagoyal9084@gmail.com>
2026-07-15 17:28:16 +0000

    datatypees

---
+++ 01_bsics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we are usind nodejs not browser
+
+console.log(3
+    +3) //code readability should be high
+console.log("Hitesh")
+
+let name="Sneha" //string  datatype
+let age=20 //number
+let isLoggedIn=false //boolean
+
+//Number=> 2 to power 53
+//BigInt  for big number
+//String => ""
+//boolean => true/false
+//null=> standalone value (intentionlly defilne kr sakte jaise let value = null)
+//undefined=>varaible declare kia hia and value define nhi kia hai
+//symbol=> to find uniqueness 
+
+//The above all were primitive datatypes
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
+
+//Object
